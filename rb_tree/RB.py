8360ddde15bf7cc11bb0c8fa5e57c84a9e20ff28
MercyFlesh <con2288@mail.ru>
2020-09-16 16:42:02 +0300

    fixed delete_balance

--- rb_tree/RB.py
+++ rb_tree/RB.py
@@ -114,142 +114,172 @@ class RB:
           
 
     def delete(self, key):
-        this_node = self.__root
-        if this_node:
-            while this_node.key != key:
-                if not this_node:
-                    return
-                if this_node.key > key:
-                    this_node = this_node.left
-                else:
-                    this_node = this_node.right
-            
-            color = this_node.red
-            original_color = this_node.red
+        delete_node = self.find(key)
+        if delete_node:
+            delete_color = delete_node.red
             self.__size -= 1
-            if not this_node.left and not this_node.right:
-                if not color:
-                        self.__balance_delete(this_node, original_color)
-                if this_node is this_node.parent.left:
-                    this_node.parent.left = None    #_node(red=False)
-                else:
-                    this_node.parent.right = None     #_node(red=False)
 
+            if not delete_node.left.key and not delete_node.right.key:
+                if delete_node is self.__root:
+                    self.__root = None
+                    return
+                
+                if delete_color == False:
+                        self.__balance_delete(delete_node)
+                if delete_node is delete_node.parent.left:
+                    delete_node.parent.left = _none_leaf(delete_node.parent)   
+                else:
+                    delete_node.parent.right = _none_leaf(delete_node.parent) 
                 return
              
-            if this_node.left and not this_node.right:
-                if not this_node.parent:
-                    self.__root = this_node.left
-                elif this_node.parent.left is this_node:
-                    this_node.parent.left = this_node.left
+            if delete_node.left.key and not delete_node.right.key:
+                if not delete_node.parent:
+                    self.__root = delete_node.left
+                elif delete_node.parent.left is delete_node:
+                    delete_node.parent.left = delete_node.left
                 else:
-                    this_node.parent.right = this_node.left
-                this_node.left.parent = this_node.parent 
-                this_node = this_node.left
-                x = this_node
+                    delete_node.parent.right = delete_node.left
+                delete_node.left.parent = delete_node.parent 
+                target_node_for_balance = delete_node.left
 
-            elif not this_node.left and this_node.right:
-                if not this_node.parent:
-                    self.root = this_node.right              
-                elif this_node is this_node.parent.left:
-                    this_node.parent.left = this_node.right
+            elif not delete_node.left.key and delete_node.right.key:
+                if not delete_node.parent:
+                    self.__root = delete_node.right              
+                elif delete_node is delete_node.parent.left:
+                    delete_node.parent.left = delete_node.right
                 else:
-                    this_node.parent.right = this_node.right
-                this_node.right.parent = this_node.parent
-                this_node = this_node.right
-                x = this_node
+                    delete_node.parent.right = delete_node.right
+                delete_node.right.parent = delete_node.parent
+                target_node_for_balance = delete_node.right
                 
             else:
-                next = this_node.left
-                if next.right:
-                    while next.right:
-                        next = next.right
-                color = next.red
-                if next is not this_node.left:
-                    next.parent.right = next.left
-                    if color == False:
-                        x = next.left
-
-                    if next.left:
-                        next.left.parent = next.parent
-                    else:
-                        x = _node(red=False, parent=next.parent)
-                            
-                    next.left = this_node.left
-                    next.left.parent =  next
-                elif next.left:
-                    x = next.left
+                next_node = delete_node.left
+                while next_node.right.key:
+                        next_node = next_node.right
+                delete_color = next_node.red
+                
+                if next_node is not delete_node.left:
+                    next_node.parent.right = next_node.left
+                    next_node.left.parent = next_node.parent
+                    if not delete_color:
+                        target_node_for_balance = next_node.left
+                    next_node.left = delete_node.left
+                    next_node.left.parent =  next_node
                 else:
-                    x = _node(red=False, parent=next)
+                    target_node_for_balance = next_node.left
                    
 
-                next.red = original_color
-                next.right = this_node.right
-                next.parent = this_node.parent
+                next_node.red = delete_node.red
+                next_node.right = delete_node.right
+                next_node.parent = delete_node.parent
 
-                if this_node is self.__root:
-                    self.__root = next
-                elif this_node is this_node.parent.left:
-                    this_node.parent.left = next
+                if delete_node is self.__root:
+                    self.__root = next_node
+                elif delete_node is delete_node.parent.left:
+                    delete_node.parent.left = next_node
                 else:
-                    this_node.parent.right = next
+                    delete_node.parent.right = next_node
 
-                this_node = next
-                next.right.parent = next
-            if not color:
-                self.__balance_delete(x, original_color)
+                delete_node.right.parent = next_node
+            if not delete_color:
+                self.__balance_delete(target_node_for_balance)
+            
+      
+    def __balance_insert(self, target_node):
+        while target_node.parent.red:
+            grandfather = target_node.parent.parent
+            if target_node.parent is grandfather.left:
+                uncle = grandfather.right
+                if uncle.key and uncle.red:
+                        target_node.parent.red = False
+                        uncle.red = False
+                        grandfather.red = True
+                        target_node = grandfather
+                else:
+                    if target_node is target_node.parent.right:
+                        target_node = target_node.parent
+                        self.__rotate_left(target_node)
+                    
+                    target_node.parent.red = False
+                    grandfather = target_node.parent.parent
+                    grandfather.red = True
+                    self.__rotate_right(grandfather)    
+            else:
+                uncle = grandfather.left
+                if uncle.red:
+                        target_node.parent.red = False
+                        uncle.red = False
+                        grandfather.red = True
+                        target_node = grandfather
+                        if target_node.parent:
+                            grandfather = target_node.parent.parent
+                else:
+                    if target_node is target_node.parent.left:
+                        target_node = target_node.parent
+                        self.__rotate_right(target_node)
+                    target_node.parent.red = False
+                    grandfather = target_node.parent.parent
+                    grandfather.red = True
+                    self.__rotate_left(grandfather)
+            
+            if target_node is self.__root:
+                break
+        self.__root.red = False
 
 
-    def __balance_delete(self, del_node, original_color):
-        while (not del_node or not del_node.red) & (del_node != self.__root):
-            if (del_node is del_node.parent.left) or (not del_node.key and not del_node.parent.left):
-                brother = del_node.parent.right
-                if brother and brother.red:
+    def __balance_delete(self, target_node):
+        while (not target_node.key or not target_node.red) and (target_node is not self.__root):
+            if (target_node is target_node.parent.left):
+                brother = target_node.parent.right
+                if brother.key and brother.red:
                     brother.red = False
-                    del_node.parent.red = True
-                    self.__rotate_left(del_node.parent)
-                    brother = del_node.parent.right
-                if (brother.left and brother.left.red) or (brother.right and brother.right.red):
-                    if brother.right and brother.right.red == False:
+                    target_node.parent.red = True
+                    self.__rotate_left(target_node.parent)
+                    brother = target_node.parent.right
+                
+                elif brother.left.red or brother.right.red:
+                    if not brother.right.red:
                         brother.left.red = False
                         brother.red = True
                         self.__rotate_right(brother)
-                        brother = del_node.parent.right
+                        brother = target_node.parent.right
 
-                    if brother.right:
+                    if brother.right.key:
                         brother.right.red = False
 
-                    brother.red = del_node.parent.red
-                    del_node.parent.red = False
-                    self.__rotate_left(del_node.parent)
-                    del_node = self.__root
+                    brother.red = target_node.parent.red
+                    target_node.parent.red = False
+                    self.__rotate_left(target_node.parent)
+                    target_node = self.__root
+                
                 else:
-                    #when childs black
                     brother.red = True
-                    del_node = del_node.parent
+                    target_node = target_node.parent
                     
             else:
-                brother = del_node.parent.left
-                if brother and brother.red:
+                brother = target_node.parent.left
+                if brother.key and brother.red:
                     brother.red = False
-                    del_node.parent.red = True
-                    self.__rotate_right(del_node.parent)
-                    brother = del_node.parent.left
-                if (brother.left and brother.left.red) or (brother.right and brother.right.red):
-                    if brother.left and not brother.left.red:
+                    target_node.parent.red = True
+                    self.__rotate_right(target_node.parent)
+                    brother = target_node.parent.left
+                
+                elif brother.left.red or brother.right.red:
+                    if not brother.left.red:
                         brother.right.red = False
                         brother.red = True
                         self.__rotate_left(brother)
-                        brother = del_node.parent.left
-                    brother.red = del_node.parent.red
-                    del_node.parent.red = False
+                        brother = target_node.parent.left
+                    brother.red = target_node.parent.red
+                    target_node.parent.red = False
                     brother.left.red = False
-                    self.__rotate_right(del_node.parent)
-                    del_node = self.__root
+                    self.__rotate_right(target_node.parent)
+                    target_node = self.__root
+                
                 else:
                     brother.red = True
-                    del_node = del_node.parent
-        del_node.red = False
+                    target_node = target_node.parent
+        target_node.red = False
         self.__root.red = False
 
 
